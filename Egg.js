dcfd3e72c1aded991dfb3ff00c4659bc82eb5f70
Gabriel  Williams <109472974+Gabriel233444@users.noreply.github.com>
2023-02-20 03:24:10 -0800

    updated Egg.js

---
+++ Egg.js
@@ -0,0 +1,209 @@
+function parseExpression(program) {
+    program = skipSpace(program);
+    let match, expr;
+    if (match = /^"([^"]*)"/.exec(program)) {
+        expr = {type: "value", value: match[1]};
+    } else if (match = /^\d+\b/.exec(program)) {
+        expr = {type: "value", value: Number(match[0])};
+    } else if (match = /^[^\s(),#"]+/.exec(program)) {
+        expr = {type: "word", name: match[0]};
+    } else {
+        throw new SyntaxError("Unexpected syntax: " + program);
+    }
+    return parseApply(expr, program.slice(match[0].length));
+}
+
+function skipSpace(string) {
+    let first = string.search(/\S/);
+    if (first == -1) return "";
+    return string.slice(first);
+}
+
+function parseApply(expr, program) {
+    program = skipSpace(program);
+    if (program[0] != "(") {
+        return {expr: expr, rest: program};
+    }
+    program = skipSpace(program.slice(1));
+    expr = {type: "apply", operator: expr, args: []};
+    while (program[0] != ")") {
+        let arg = parseExpression(program);
+        expr.args.push(arg.expr);
+        program = skipSpace(arg.rest);
+        if (program[0] == ",") {
+            program = skipSpace(program.slice(1));
+        } else if (program[0] != ")") {
+            throw new SyntaxError("Expected ',' or ')'");
+        }
+    }
+    return parseApply(expr, program.slice(1));
+}
+
+function parse(program) {
+    let {expr, rest} = parseExpression(program);
+    if (skipSpace(rest).length > 0) {
+        throw new SyntaxError("Unexpected text after program");
+    }
+    return expr;
+}
+console.log(parse("+(a, 10)"));
+
+const specialForms = Object.create(null);
+
+function evaluate(expr, scope) {
+    if (expr.type == "value") {
+        return expr.value;
+    } else if (expr.type == "word") {
+        if (expr.name in scope) {
+            return scope[expr.name];
+        } else {
+            throw new ReferenceError(
+                `Undefined binding: ${expr.name}`);
+            }
+    } else if (expr.type == "apply") {
+        let {operator, args} = expr;
+        if (operator.type == "word" && operator.name in specialForms) {
+            return specialForms[operator.name](expr.args, scope);
+        } else {
+            let op = evaluate(operator, scope);
+            if (typeof op == "function") {
+                return op(...args.map(arg => evaluate(arg, scope)));
+            } else {
+                throw new TypeError("Applying a non-function.");
+            }
+        }
+    }
+}
+
+specialForms.if = (args, scope) => {
+    if (args.length != 3) {
+        throw new SyntaxError("Wrong number of args to if");
+    } else if (evaluate(args[0], scope) !== false) {
+        return evaluate(args[1], scope);
+    } else {
+        return evaluate(args[2], scope);
+    }
+};
+
+specialForms.while = (args, scope) => {
+    if (args.length != 2) {
+        throw new SyntaxError("Wrong number of args to while");
+    }
+    while (evaluate(args[0], scope) !== false) {
+        evaluate(args[1], scope);
+    }
+    // Since undefined does not exist in Egg, we return false,
+    // for lack of a meaningful result.
+    return false;
+};
+
+specialForms.do = (args, scope) => {
+    let value = false;
+    for (let arg of args) {
+        value = evaluate(arg, scope);
+    }
+    return value;
+};
+
+specialForms.define = (args, scope) => {
+    if (args.length != 2 || args[0].type != "word") {
+        throw new SyntaxError("Incorrect use of define");
+    }
+    let value = evaluate(args[1], scope);
+    scope[args[0].name] = value;
+    return value;
+};
+
+//Since there are only two Boolean values, we do not need special
+//syntax for them. We simply bind two names to the values true and false and
+//use them.
+const topScope = Object.create(null);
+topScope.true = true;
+topScope.false = false;
+
+//We can now evaluate a simple expression that negates a Boolean value.
+let prog = parse(`if(true, false, true)`);
+console.log(evaluate(prog, topScope));
+// → false
+
+//and also do more
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+    topScope[op] = Function("a, b", `return a ${op} b;`);
+}
+
+//A way to output values is also useful, so we wrap console.log in a function
+//and call it print.
+topScope.print = value => {
+    console.log(value);
+    217
+    return value;
+};
+
+function run(program) {
+    return evaluate(parse(program), Object.create(topScope));
+}
+
+//viola
+run(`do(define(total, 0),
+        define(count, 1),
+        while(<(count, 11),
+            do(define(total, +(total, count)),
+                define(count, +(count, 1)))),
+        print(total))
+`);
+// → 55
+
+//A programming language without functions is a poor programming language
+//indeed.
+//Fortunately, it isn’t hard to add a fun construct, which treats its last argument as the function’s body and uses all arguments before that as the names
+//of the function’s parameters.
+specialForms.fun = (args, scope) => {
+    if (!args.length) {
+        throw new SyntaxError("Functions need a body");
+    }
+    let body = args[args.length - 1];
+    let params = args.slice(0, args.length - 1).map(expr => {
+        if (expr.type != "word") {
+            throw new SyntaxError("Parameter names must be words");
+        }
+        return expr.name;
+    });
+    return function() {
+        if (arguments.length != params.length) {
+            throw new TypeError("Wrong number of arguments");
+        }
+        let localScope = Object.create(scope);
+        for (let i = 0; i < arguments.length; i++) {
+            localScope[params[i]] = arguments[i];
+        }
+        return evaluate(body, localScope);
+    };
+};   
+
+//Functions in Egg get their own local scope. The function produced by the
+//fun form creates this local scope and adds the argument bindings to it. It then
+//evaluates the function body in this scope and returns the result.
+run(`
+do(define(plusOne, fun(a, +(a, 1))),
+    print(plusOne(10)))
+`);
+// → 11
+
+run(`
+do(define(pow, fun(base, exp,
+    if(==(exp, 0),
+        1,
+        *(base, pow(base, -(exp, 1)))))),
+    print(pow(2, 10)))
+`);
+// → 1024
+
+run(`
+do(define(f, fun(a, fun(b, +(a, b)))),
+    print(f(4)(5)))
+`);
+// → 9
+
+    
+    
+    
